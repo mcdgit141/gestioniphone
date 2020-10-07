5c0d763e7ec01177435d56378a5662d017fba7b8
mcdgit141 <mcdmail77@gmail.com>
2020-10-07 13:25:48 +0200

    commit avant pull

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,35 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormsModule } from '@angular/forms' 
+ 
+import { ReactiveFormsModule} from '@angular/forms' 
 
 @Component({
 	selector: 'login',
-	template: `
-    <div class='row'>
-    <div class="col s12 m4 offset-m4">
-    <div class="card hoverable">
-      <div class="card-content center">
-          <span class="card-title">Page de connexion</span>
-          <p><em>{{message}}</em></p>
-      </div>
-			<form #loginForm="ngForm">
-	      <div>
-					<label for="name">Name</label>
-	        <input type="text" id="name" [(ngModel)]="name" name="name" required>
-	      </div>
-	      <div>
-	        <label for="password">Password</label>
-	        <input type="password" id="password" [(ngModel)]="password" name="password" required>
-	      </div>
-	    </form>
-      <div class="card-action center">
-        <a (click)="login()" class="waves-effect waves-light btn"  *ngIf="!authService.isLoggedIn">Se connecter</a>
-        <a (click)="logout()" *ngIf="authService.isLoggedIn">Se déconnecter</a>
-      </div>
-    </div>
-    </div>
-    </div>
-  `
+	templateUrl: './login.component.html'
 })
 export class LoginComponent {
 	message: string = 'Vous êtes déconnecté. (pikachu/pikachu)';
@@ -38,9 +16,9 @@ export class LoginComponent {
 	private password: string;
 
 	constructor(
-		private authService: AuthService,
-		private router: Router,
-		private titleService: Title) { }
+		public authService: AuthService,
+		public router: Router,
+		public titleService: Title) { }
 
 	ngOnInit() {
 		this.titleService.setTitle('Connexion');
