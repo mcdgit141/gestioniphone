7881eee69e79b018314af26a93cb73d1b302e007
Leopold KAMDEM DJOKO <kamdemleo@yahoo.fr>
2020-10-08 17:26:42 +0200

    suite formulaire creation utilisateur

--- src/app/container/utilisateurs-container/utilisateurs-form/utilisateurs-form.component.ts
+++ src/app/container/utilisateurs-container/utilisateurs-form/utilisateurs-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Utilisateur } from 'src/app/models/Utilisateur';
 
 @Component({
   selector: 'app-utilisateurs-form',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./utilisateurs-form.component.scss']
 })
 export class UtilisateursFormComponent implements OnInit {
+  
+  utilisateur;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.utilisateur = this.fb.group(
+			{
+				role: ['', Validators.required] ,
+        uid: ['', Validators.required],
+			}
+
+		)
   }
 
+
+  creerUtilisateur(utilisateur) {
+    console.log("donnee du formulaire de création de l'utilisateur",utilisateur.value);
+  }
 }
