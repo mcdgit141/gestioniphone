1bd6d8b94f0aa90ca4ca719051306a56cfeed66f
mcdgit141 <mcdmail77@gmail.com>
2020-10-13 11:12:01 +0200

    Gestion erreur 404 sur saisie URL

---
+++ src/app/erreur404/erreur404.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-erreur404',
+  templateUrl: './erreur404.component.html',
+  styleUrls: ['./erreur404.component.scss']
+})
+export class Erreur404Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
