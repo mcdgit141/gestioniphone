a0dec6c4d51e8738a36e2e402cbf5a51e2bf9512
Leopold KAMDEM DJOKO <kamdemleo@yahoo.fr>
2020-10-14 16:49:28 +0200

    utilisation catchError dans httpErrInterceptor

--- src/app/services/interceptors/httperr.interceptor.ts
+++ src/app/services/interceptors/httperr.interceptor.ts
@@ -13,50 +13,55 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable()
 export class HttperrInterceptor implements HttpInterceptor {
-
+  
   constructor(private route:Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log(" *** 7- j'entre dans HttpErrorInterceptor");
+    console.log("*** je suis dans httperror interceptor***")
     return next.handle(request).pipe(
-     tap(
-        (error:any) => {
-          if (error instanceof HttpErrorResponse) {
-            switch (error.status) {
-              case 0:
-                  alert("Serveur non disponible : Rééssayer ultérieurement");
+      
+      catchError(
+        (error) => {
+          console.log(typeof error);      
+        if (error instanceof HttpErrorResponse) {
+          switch (error.status) {
+            case 0:
+                alert("Serveur non disponible : Rééssayer ultérieurement");
+             
+            break;
+            case 400:
+              alert("Informations incorrectes transmises dans la requête");
+              
               break;
-              case 400:
-                alert("Informations incorrectes transmises dans la requête");
-                
-                break;
-              case 401:
-                alert("erreur d'authentification, merci de vous authentifier à nouveau");
-                this.route.navigate(["/login"]);
-                break;
-              case 403:
-                alert(error.error.message);
-                break;
-              case 404:
-                alert("La ressource demandé n'a pas été trouvé");
-                
-                break;
-              case 409:
-                 alert("Action impossible, car déjà réalisé");
-                 break;
-              case 500:
-                alert("Erreur serveur : Rééssayez ultérieurement");
-                break;
-              default:
-                alert("Erreur serveur : Rééssayez ultérieurement");
-                break;
-            }
-          } 
-          else {
-            console.log("erreur autre que HttpErrorResponse", error);
+            case 401:
+              alert("erreur d'authentification, merci de vous authentifier à nouveau");
+              this.route.navigate(["/login"]);
+              
+              break;
+            case 403:
+              alert(error.error.message);
+              break;
+            case 404:
+              alert("La ressource demandé n'a pas été trouvé");
+              
+              break;
+            case 409:
+               alert("Action impossible, car déjà réalisé");
+               
+               break;
+            case 500:
+              alert("Erreur serveur : Rééssayez ultérieurement");
+             
+              break;
+            default:
+              alert("Erreur serveur : Rééssayez ultérieurement");
+              break;
           }
+        } else {
+          console.log("erreur autre que HttpErrorResponse");
         }
-      )
+          return throwError(error);    
+      })
     )
   }
 }
