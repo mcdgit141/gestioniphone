1e43e8c3e183606604c7b689299aab0417b54ad3
Leopold KAMDEM DJOKO <kamdemleo@yahoo.fr>
2020-10-14 15:46:25 +0200

    correction httpErrInt + suppr commentaire

--- src/app/services/affectation.service.ts
+++ src/app/services/affectation.service.ts
@@ -9,130 +9,60 @@ import { Affectation } from '../models/affectation';
 })
 export class AffectationService {
 
-  private API=environment.API_URL;
+  private API = environment.API_URL;
   authService: any;
-  
+
   private _affectationSubject = new BehaviorSubject<Affectation[]>([]);
   private _affectationMetaSubject = new BehaviorSubject({});
 
-   // _affectationSubject = new BehaviorSubject<[]>([]);
-  readonly affectations$:Observable<Affectation[]> = this._affectationSubject.asObservable();
-  readonly affectationMeta$:Observable<any> = this._affectationMetaSubject.asObservable();
+  readonly affectations$: Observable<Affectation[]> = this._affectationSubject.asObservable();
+  readonly affectationMeta$: Observable<any> = this._affectationMetaSubject.asObservable();
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  createAffectation(mesdata)  {
-    //  console.log("mesdatadansservicecreation--" + formatJsonMesDatas);
-      //return this.http.post(this.API+"/affectation/creation",mesdata);
-
-      //await this.http.post(this.API+"/affectation/creation",formatJsonMesDatas).toPromise().then(
-     //  (response:any) => alert("affectation créée"))
-    //
-     // }
-
-
-      this.http.post(this.API+"/affectation/creation",mesdata)
+  createAffectation(mesdata) {
+    this.http.post(this.API + "/affectation/creation", mesdata)
       .subscribe(
-        () => {
-          console.log('Enregistrement terminé !');
-          alert("affectation créée");
-        },
-        (error:HttpErrorResponse) => {
-          console.log('Erreur ! : ' , error.status); }
-        );
-        }
+        () => alert("affectation créée")
+      );
+  }
 
-        
-    getAffectations(param) {
-      console.log("getAffectations - entre");
-            
-      // let param1 = {
-      //     "uid": "100200",
-      // //    "uid": null,
-      //       "nom": null,
-      //       "codeUo": null,
-      //       "nomUsageUo": null,
-      //       "nomSite": null,
-      //       "numeroLigneCollaborateur": null,
-      //       "nomModeleIphone": null,
-      //       "dateRenouvMin": null,
-      //       // "dateRenouvMin": "2020-07-07",
-      //       "dateRenouvMax": null,
-      //       // "taillePage": 5,
-      //       "taillePage": null,
-      //       "numeroDePage": 1,
-      //       "critereDeTri": "UID",
-      //       "sensduTri": "D"
-      // }
 
-      console.log(this._affectationSubject);
-     //  this._affectationSubject.subscribe(
-        //  data => { if (data.length === 0) {
-        //  data =>  {
+  getAffectations(param) {
+    this.http.post(this.API + "/affectation/liste2", param).subscribe(
+      (data: any) => {
+        this._affectationSubject.next(data.datas)
+        this._affectationMetaSubject.next(data.metadata)
+      }
+    )
+    return this._affectationSubject;
 
-            console.log("getAffectations param - avant post********--" , param);
-             this.http.post(this.API+"/affectation/liste2",param).subscribe(
-                (data:any) => {
-                   console.log(data);
-                   console.log(data.metadata);
-                   console.log(data.datas);
-                 //   let affectation = data[''];
-                 //   console.log(affectation);
-                   
-                   this._affectationSubject.next(data.datas)
-                   this._affectationMetaSubject.next(data.metadata)
-                }
-                )
-        // }
-        //  }
-     //  )
-      console.log(this._affectationSubject);
+  }
 
-      return this._affectationSubject;
-      
-   }
 
-  
-   cloturerAffectation(mesdatacloture)  {
-    
-    console.log("dans cloturerAffectation--mesdatacloture*****---", mesdatacloture);
-       
-    this.http.put(this.API+"/affectation/cloture",mesdatacloture)
+  cloturerAffectation(mesdatacloture) {
+    console.log("****4- methode cloturerAffectatoin dans le serviceAffectation")
+    this.http.put(this.API + "/affectation/cloture", mesdatacloture)
       .subscribe(
-        () => {
-          console.log('clôture effectuée !');
-          alert("affectation clôturée");
-        },
-        (error:HttpErrorResponse) => {
-          console.log('Erreur sur la clôture ! : ' , error.status); }
-        );
-        }
+        () => alert("affectation clôturée")
+      );
+  }
 
-    supprimerAffectation(numeroaffectationasupprimer,commentaireasupprimer)  {
+  supprimerAffectation(numeroaffectationasupprimer, commentaireasupprimer) {
 
-      console.log("dans supprimer affectation---commentaireasupprimer*************", commentaireasupprimer);
-      let mot1='"';
-      let commentaireaenvoyer = commentaireasupprimer;
-      let commentaire = mot1+commentaireaenvoyer+mot1;
-      let commentaire1 = commentaire.toString();
-      console.log("dans supprimer affectation---commentaire ***********",commentaire);
-      console.log("dans supprimer affectation---commentaire1 en toString ***********",commentaire1);
+    let mot1 = '"';
+    let commentaireaenvoyer = commentaireasupprimer;
+    let commentaire = mot1 + commentaireaenvoyer + mot1;
+    let commentaire1 = commentaire.toString();
 
-      let deleteParams= new HttpParams().set("id",numeroaffectationasupprimer)
-                                        .set("commentaire",commentaire1);
+    let deleteParams = new HttpParams().set("id", numeroaffectationasupprimer)
+      .set("commentaire", commentaire1);
 
-     
-     console.log("dans supprimer affectation---deleteParams---", deleteParams);
-      this.http.delete(this.API+"/affectation/suppression",{params:deleteParams})
-          .subscribe(
-              () => {
-                console.log('suppression effectuée !');
-                alert("affectation supprimée");
-              },
-              (error:HttpErrorResponse) => {
-                console.log('Erreur sur la suppression! : ' , error.status); }
-              );
-              }
-      
+    this.http.delete(this.API + "/affectation/suppression", { params: deleteParams })
+      .subscribe(
+        () => alert("affectation supprimée")
+      );
+  }
+
 }
 
--- src/app/services/interceptors/httperr.interceptor.ts
+++ src/app/services/interceptors/httperr.interceptor.ts
@@ -6,8 +6,8 @@ import {
   HttpInterceptor,
   HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, finalize, retry, tap } from 'rxjs/operators';
 import { error } from 'protractor';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -17,48 +17,46 @@ export class HttperrInterceptor implements HttpInterceptor {
   constructor(private route:Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log(" *** 7- j'entre dans HttpErrorInterceptor");
     return next.handle(request).pipe(
-      
-      catchError((error) => {      
-        if (error instanceof HttpErrorResponse) {
-          switch (error.status) {
-            case 0:
-                alert("Serveur non disponible : Rééssayer ultérieurement");
-             
-            break;
-            case 400:
-              alert("Informations incorrectes transmises dans la requête");
-              
+     tap(
+        (error:any) => {
+          if (error instanceof HttpErrorResponse) {
+            switch (error.status) {
+              case 0:
+                  alert("Serveur non disponible : Rééssayer ultérieurement");
               break;
-            case 401:
-              alert("erreur d'authentification, merci de vous authentifier à nouveau");
-              this.route.navigate(["/login"]);
-              
-              break;
-            case 403:
-              alert(error.error.message);
-              break;
-            case 404:
-              alert("La ressource demandé n'a pas été trouvé");
-              
-              break;
-            case 409:
-               alert("Action impossible, car déjà réalisé");
-               
-               break;
-            case 500:
-              alert("Erreur serveur : Rééssayez ultérieurement");
-             
-              break;
-            default:
-              alert("Erreur serveur : Rééssayez ultérieurement");
-              break;
+              case 400:
+                alert("Informations incorrectes transmises dans la requête");
+                
+                break;
+              case 401:
+                alert("erreur d'authentification, merci de vous authentifier à nouveau");
+                this.route.navigate(["/login"]);
+                break;
+              case 403:
+                alert(error.error.message);
+                break;
+              case 404:
+                alert("La ressource demandé n'a pas été trouvé");
+                
+                break;
+              case 409:
+                 alert("Action impossible, car déjà réalisé");
+                 break;
+              case 500:
+                alert("Erreur serveur : Rééssayez ultérieurement");
+                break;
+              default:
+                alert("Erreur serveur : Rééssayez ultérieurement");
+                break;
+            }
+          } 
+          else {
+            console.log("erreur autre que HttpErrorResponse", error);
           }
-        } else {
-          console.log("erreur autre que HttpErrorResponse");
         }
-          return next.handle(request);    
-      })
+      )
     )
   }
 }
